5a19a0c2e830083dc4b9ed88d182361f483d8cca
wk6577 <dsdkakjlasdf@naver.com>
2025-04-24 10:49:36 +0900

    config 추가

---
+++ src/main/java/com/milestone/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.milestone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // React 앱이 실행되는 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
